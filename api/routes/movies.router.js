a906ad42c09c944f882d9f81492d172a8faee095
fitodac <fitodac@gmail.com>
2023-06-20 18:55:35 -0300

    Api mini fix

--- api/routes/movies.router.js
+++ api/routes/movies.router.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 
-const upload = multer({ dest: '../uploads/' })
+const upload = multer({ dest: 'uploads/' })
 
 const { getMovies, getMovie } = require('../controllers/getMovies')
 const addMovie = require('../controllers/addMovie')
