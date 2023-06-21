e8913886123dd2837c9bb4bc511a40a9751c7be2
fitodac <fitodac@gmail.com>
2023-06-21 02:09:57 -0300

    Trying deploy 2

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const { app } = require('./server.js')
 require('dotenv').config()
 const port = process.env.API_PORT || 3000
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log(`http://localhost:${port}`)
 })
 
